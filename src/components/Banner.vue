b51ab28ad53230a68e25ec435b18ac12b51bc200
Brian Darley <bdarley@bdarley.net>
2018-02-11 14:38:35 -0500

    Add project files.

---
+++ src/components/Banner.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+  <div class="banner">
+    <div class="banner-container">
+      <div class="banner-overlay"></div>
+      <img src="../assets/img/UNC_logo_white.png" class="unc-logo" />
+    </div>
+    <div class="row">
+      <div class="offset-8 col-md-4">
+        <div class="application">
+          <div class="department">
+            ITS Workgroup Services
+          </div>
+          <div class="app-name">
+            Self Service
+          </div>
+          <div class="features">
+            Active Directory,Exchange,Heelmail,Listserv,MassMail
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="mnu">
+      <ul class="list-inline">
+        <li class="list-inline-item btn btn-primary">Home</li>
+        <li class="list-inline-item btn btn-primary">List Manager</li>
+        <li class="list-inline-item btn btn-primary">Mass Mail</li>
+        <li class="list-inline-item btn btn-primary">Win Tools</li>
+        <li class="list-inline-item btn btn-primary">Office365 Groups</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/SelfService.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+  <banner/>
+    <div class="main-vue">
+      <div class="row">
+        <div class="col-sm-3">
+          <ul class="app-pages list-group">
+            <li class="list-group-item btn btn-primary active"><i class='fa fa-chevron-right'></i> Welcome</li>
+            <li class="list-group-item btn btn-primary"> Email Sign-Up</li>
+            <li class="list-group-item btn btn-primary">Listserv Subscriptions</li>
+            <li class="list-group-item btn btn-primary">My Aliases</li>
+            <li class="list-group-item btn btn-primary">My AD Groups</li>
+            <li class="list-group-item btn btn-primary">My Resources</li>
+          </ul>
+        </div>
+        <div class="col-sm-9">
+          <p class="py-4">
+  Welcome to the ITS Email Self Service Site. Use the links on this site to perform actions such as creating an email account, requesting an alias or automating calendar events. Some users will get an Exchange account and some will get a Heelmail account. 
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import banner from './Banner.vue'
+
+export default {
+  name: 'SelfService',
+  data () {
+    return {
+      msg: 'SelfService'
+    }
+  },
+  created () {},
+  computed: { },
+  methods: { },
+  components: {
+    banner
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
